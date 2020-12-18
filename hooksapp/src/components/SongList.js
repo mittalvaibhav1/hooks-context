9a51d6a826c2c67da27bea220bf5869f17a8f137
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-12-19 01:00:49 +0530

    modifyState

--- hooksapp/src/components/SongList.js
+++ hooksapp/src/components/SongList.js
@@ -1,11 +1,14 @@
 import React , {useState} from 'react';
-
+import uuid from 'uuid/v1';
 const SongList = () => {
     const[songs, setSongs] = useState([
         {title: 'Almost Home', id: 1},
         {title: 'Memory Gospel', id: 2},
         {title: 'This Wild Darkness', id: 3}
     ]);
+    const addSong = () => {
+        setSongs([...songs,{title: 'New Song' , id : uuid() }]);
+    }
     return ( 
         <div className="song-list">
             <ul>
@@ -13,6 +16,7 @@ const SongList = () => {
                     return (<li key = {id}>{title}</li>);
                 })}
             </ul>
+            <button onClick={addSong}>Add a Song!</button>
         </div>
      );
 }
